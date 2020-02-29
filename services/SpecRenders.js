1376267ec6c302229a7a0c8beeec3b5aae1ff9a9
volpe28v <naoki.koda@gmail.com>
2020-03-01 01:40:41 +0900

    before の追加と jest の終端を修正

--- services/SpecRenders.js
+++ services/SpecRenders.js
@@ -4,7 +4,8 @@ export default [
     symbols: {
       d: (body) => `describe '${body}' do`,
       c: (body) => `context '${body}' do`,
-      i: (body) => `xit '${body}' do`
+      i: (body) => `xit '${body}' do`,
+      b: (body) => `before do ${body.length !== 0 ? '# ' + body : ''}`
     },
     comment: (body) => `# ${body}`,
     terminal: 'end'
@@ -14,9 +15,10 @@ export default [
     symbols: {
       d: (body) => `describe('${body}', () => {`,
       c: (body) => `describe('${body}', () => {`,
-      i: (body) => `it('${body}', () => {`
+      i: (body) => `it('${body}', () => {`,
+      b: (body) => `beforeEach(() => { ${body.length !== 0 ? '// ' + body : ''}`
     },
     comment: (body) => `// ${body}`,
-    terminal: '}'
+    terminal: '})'
   }
 ]
